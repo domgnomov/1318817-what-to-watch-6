3f8b0f204e8be53bcabe03d26161f25bb25d8343
nivanov <nivanov@phoenixit.ru>
2021-02-17 12:46:49 +0300

    module4-task1

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {useHistory} from 'react-router-dom';
 import {FilmValidation} from "../validation/validation";
 
 const Card = (props) => {
   const {film, setActiveId} = props;
 
+  const cardRef = useRef();
+
   const history = useHistory();
+
+  useEffect(() => {
+    const delay = function (elem, callback) {
+      let timeout = null;
+      elem.onmouseover = function () {
+        timeout = setTimeout(callback, 1000);
+      };
+
+      elem.onmouseout = function () {
+        clearTimeout(timeout);
+      };
+    };
+
+    delay(cardRef.current, function () {
+      console.log("Fired");
+    });
+
+    return () => {
+      cardRef.current.onmouseover = null;
+      cardRef.current.onmouseout = null;
+    };
+  }, []);
 
   return (
     <>
       <article className="small-movie-card catalog__movies-card" >
-        <div className="small-movie-card__image" onMouseEnter={() => setActiveId(film.id)}>
+        <div className="small-movie-card__image" ref={cardRef} onMouseEnter={() => setActiveId(film.id)}>
           <img src={film.posterImage} alt={film.name} width="280" height="175"
           />
         </div>
