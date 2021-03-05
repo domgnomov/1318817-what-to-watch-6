dbac82212504466f0cdc5911c003a68b8dd6edb9
nivanov <nivanov@phoenixit.ru>
2021-03-05 11:54:08 +0300

    module7-task1

--- src/store/action.js
+++ src/store/action.js
@@ -1,4 +1,5 @@
 import {DEFAULT_GENRE} from "../const";
+import {createAction} from "@reduxjs/toolkit/";
 
 export const ActionType = {
   CHANGE_GENRE: `changeGenre`,
@@ -14,64 +15,51 @@ export const ActionType = {
   SET_AUTH_INFO: `setAuthInfo`
 };
 
-export const changeGenre = (genre) => ({
-  type: ActionType.CHANGE_GENRE,
-  payload: genre
+export const changeGenre = createAction(ActionType.CHANGE_GENRE, (genre) => {
+  return {payload: genre};
 });
 
-export const getFilms = (genre, allFilms, showCount) => {
+export const getFilms = createAction(ActionType.GET_FILMS, (genre, allFilms, showCount) => {
   const filteredFilms = genre === DEFAULT_GENRE ? allFilms : Array.from(allFilms).filter((film) => film.genre === genre);
   const showedFilms = filteredFilms.slice(0, showCount);
   return {
-    type: ActionType.GET_FILMS,
     payload: showedFilms
   };
-};
-
-export const loadFilms = (films) => ({
-  type: ActionType.LOAD_FILMS,
-  payload: films
 });
 
-export const loadFilm = (film) => ({
-  type: ActionType.LOAD_FILM,
-  payload: film
+export const loadFilms = createAction(ActionType.LOAD_FILMS, (films) => {
+  return {payload: films};
 });
 
-export const requireAuthorization = (status) => ({
-  type: ActionType.REQUIRED_AUTHORIZATION,
-  payload: status,
+export const loadFilm = createAction(ActionType.LOAD_FILM, (film) => {
+  return {payload: film};
 });
 
-export const setAuthInfo = (authInfo) => ({
-  type: ActionType.SET_AUTH_INFO,
-  payload: authInfo,
+export const requireAuthorization = createAction(ActionType.REQUIRED_AUTHORIZATION, (status) => {
+  return {payload: status};
 });
 
-export const changeShowCount = (showCount) => ({
-  type: ActionType.CHANGE_SHOW_COUNT,
-  payload: showCount
+export const setAuthInfo = createAction(ActionType.SET_AUTH_INFO, (authInfo) => {
+  return {payload: authInfo};
 });
 
-export const changeAllFilmsByActiveGenre = (genre, allFilms) => {
+export const changeShowCount = createAction(ActionType.CHANGE_SHOW_COUNT, (showCount) => {
+  return {payload: showCount};
+});
+
+export const changeAllFilmsByActiveGenre = createAction(ActionType.CHANGE_ALL_FILMS_BY_ACTIVE_GENRE, (genre, allFilms) => {
   const allFilmsByActiveGenre = genre === DEFAULT_GENRE ? allFilms : Array.from(allFilms).filter((film) => film.genre === genre);
-  return {
-    type: ActionType.CHANGE_ALL_FILMS_BY_ACTIVE_GENRE,
-    payload: allFilmsByActiveGenre
-  };
-};
-
-export const redirectToRoute = (url) => ({
-  type: ActionType.REDIRECT_TO_ROUTE,
-  payload: url,
+  return {payload: allFilmsByActiveGenre};
 });
 
-export const redirectToFilm = (url) => ({
-  type: ActionType.REDIRECT_TO_FILM,
-  payload: url,
+export const redirectToRoute = createAction(ActionType.REDIRECT_TO_ROUTE, (url) => {
+  return {payload: url};
 });
 
-export const redirectToNotFound = (url) => ({
-  type: ActionType.REDIRECT_TO_NOT_FOUND,
-  payload: url,
+export const redirectToFilm = createAction(ActionType.REDIRECT_TO_FILM, (url) => {
+  return {payload: url};
+});
+
+export const redirectToNotFound = createAction(ActionType.REDIRECT_TO_NOT_FOUND, (url) => {
+  return {payload: url};
 });
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,13 @@
-import {ActionType} from "./action";
+import {
+  changeAllFilmsByActiveGenre,
+  changeGenre,
+  changeShowCount,
+  getFilms, loadFilm,
+  loadFilms, requireAuthorization,
+  setAuthInfo
+} from "./action";
 import {AuthorizationStatus, DEFAULT_GENRE, DEFAULT_AUTH_INFO, SHOW_MORE_DEFAULT_COUNT, DEFAULT_FILM} from "../const";
+import {createReducer} from "@reduxjs/toolkit/";
 
 const initialState = {
   activeGenre: DEFAULT_GENRE,
@@ -13,60 +21,42 @@ const initialState = {
   authInfo: DEFAULT_AUTH_INFO
 };
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ActionType.CHANGE_GENRE:
-      return {
-        ...state,
-        activeGenre: action.payload
-      };
+const rootReducer = createReducer(initialState, (builder) => {
+  builder.addCase(changeGenre, (state, action) => {
+    state.activeGenre = action.payload;
+  });
 
-    case ActionType.GET_FILMS:
-      return {
-        ...state,
-        filteredFilms: action.payload
-      };
+  builder.addCase(getFilms, (state, action) => {
+    state.filteredFilms = action.payload;
+  });
 
-    case ActionType.CHANGE_SHOW_COUNT:
-      return {
-        ...state,
-        showCount: action.payload
-      };
+  builder.addCase(changeShowCount, (state, action) => {
+    state.showCount = action.payload;
+  });
 
-    case ActionType.CHANGE_ALL_FILMS_BY_ACTIVE_GENRE:
-      return {
-        ...state,
-        allFilmsByActiveGenre: action.payload
-      };
+  builder.addCase(changeAllFilmsByActiveGenre, (state, action) => {
+    state.allFilmsByActiveGenre = action.payload;
+  });
 
-    case ActionType.SET_AUTH_INFO:
-      return {
-        ...state,
-        authInfo: action.payload
-      };
+  builder.addCase(setAuthInfo, (state, action) => {
+    state.authInfo = action.payload;
+  });
 
-    case ActionType.LOAD_FILMS:
-      return {
-        ...state,
-        allFilms: action.payload,
-        allFilmsByActiveGenre: action.payload,
-        filteredFilms: Array.from(action.payload).slice(0, SHOW_MORE_DEFAULT_COUNT),
-        isDataLoaded: true
-      };
+  builder.addCase(loadFilms, (state, action) => {
+    state.allFilms = action.payload;
+    state.allFilmsByActiveGenre = action.payload;
+    state.filteredFilms = Array.from(action.payload).slice(0, SHOW_MORE_DEFAULT_COUNT);
+    state.isDataLoaded = true;
+  });
 
-    case ActionType.LOAD_FILM:
-      return {
-        ...state,
-        currentFilm: action.payload,
-      };
+  builder.addCase(loadFilm, (state, action) => {
+    state.currentFilm = action.payload;
+  });
 
-    case ActionType.REQUIRED_AUTHORIZATION:
-      return {
-        ...state,
-        authorizationStatus: action.payload,
-      };
-  }
-  return state;
-};
+  builder.addCase(requireAuthorization, (state, action) => {
+    state.authorizationStatus = action.payload;
+  });
 
-export {reducer};
+});
+
+export {rootReducer};
