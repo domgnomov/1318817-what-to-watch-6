00da8d77a6f6e6bf0f51d3cae8f8fefd8698dfa7
nivanov <nivanov@phoenixit.ru>
2021-03-09 14:58:19 +0300

    module7-task2

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -2,7 +2,7 @@ import React, {useRef, useEffect} from 'react';
 import {useHistory, useParams} from 'react-router-dom';
 import {useDispatch, useSelector} from "react-redux";
 import PlayPause from "./play-pause";
-import {setPlayingStatus} from "../../store/action";
+import {setPlayingStatus, setPlayingTime} from "../../store/action";
 import TimeLapse from "./time-lapse";
 import {fetchFilm} from "../../store/api-actions";
 
@@ -16,13 +16,14 @@ const Player = () => {
   const {id} = useParams();
   console.log(`Player rerender`);
 
+  const film = currentFilm;
+  let lastTime = 0;
+
   useEffect(() => {
     dispatch(fetchFilm(id));
   }, [id]);
 
-  const film = currentFilm;
-
-  const handlePlayPauseClick = React.useCallback(() => {
+  const handlePlayPauseClick = () => {
     const elem = videoRef.current;
     dispatch(setPlayingStatus(elem.paused));
     if (elem.paused) {
@@ -30,12 +31,44 @@ const Player = () => {
     } else {
       elem.pause();
     }
-  });
+  };
+
+  const handleFullscreenClick = (evt) => {
+    evt.preventDefault();
+    const elem = videoRef.current;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    }
+  };
+
+  const timeUpdateHandler = () => {
+    const time = Math.floor((videoRef.current.duration - videoRef.current.currentTime) % 60);
+    if (lastTime !== time) {
+      lastTime = time;
+      dispatch(setPlayingTime(format(time)));
+    }
+  };
+
+  function format(s) {
+    let h = Math.floor(s / 60 / 60);
+    h = (h >= 10) ? h : `0` + h;
+    let m = Math.floor(s / 60);
+    m = (m >= 10) ? m : `0` + m;
+    s = Math.floor(s % 60);
+    s = (s >= 10) ? s : `0` + s;
+    return h + `:` + m + `:` + s;
+  }
 
   return (
     <>
       <div className="player">
-        <video autoPlay ref={videoRef} src={film.videoLink} className="player__video" poster={film.previewImage} />
+        <video autoPlay ref={videoRef} src={film.videoLink} className="player__video" poster={film.previewImage} onTimeUpdate={timeUpdateHandler}/>
         <button type="button" className="player__exit" onClick={() => {
           videoRef.current.pause();
           history.push(`/`);
@@ -53,7 +86,7 @@ const Player = () => {
               <PlayPause/>
             </button>
             <div className="player__name">Transpotting</div>
-            <button type="button" className="player__full-screen" >
+            <button type="button" className="player__full-screen" onClick={handleFullscreenClick}>
               <svg viewBox="0 0 27 27" width={27} height={27}>
                 <use xlinkHref="#full-screen" />
               </svg>
