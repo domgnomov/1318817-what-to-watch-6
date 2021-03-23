9d900db995252caf852d52d6777dcf5dd2a72b20
nivanov <nivanov@phoenixit.ru>
2021-03-23 14:48:05 +0300

    fix

--- src/components/player/mini-player.jsx
+++ src/components/player/mini-player.jsx
@@ -6,7 +6,7 @@ const MiniPlayer = (props) => {
 
   return (
     <>
-      <video autoPlay={true} muted={true} controls width="280" height="175">
+      <video autoPlay={true} muted={true} width="280" height="175">
         <source src={film.previewVideoLink} type="video/mp4"/>
       </video>
     </>
